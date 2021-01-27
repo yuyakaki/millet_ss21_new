49c8b558d1d34e51602ae96132e85f519aea284c
yuyakakizono <yuya.kakizono@one-proof.com>
2021-01-27 17:45:29 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
-var gulp = require('gulp');
-var rename = require("gulp-rename");
-var ejs = require("gulp-ejs");
-var replace = require("gulp-replace");
-var notify = require("gulp-notify");
-var plumber = require("gulp-plumber");
-// var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const rename = require("gulp-rename");
+const ejs = require("gulp-ejs");
+const replace = require("gulp-replace");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+// const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const changed = require('gulp-changed');
 
 //setting : paths
-var paths = {
+const paths = {
   'root': './dist/',
-  'html': './dist/*.html',
-  // 'cssSrc': './src/scss/**/*.scss',
-  // 'cssDist': './dist/css/',
-  'jsSrc': './src/js/**/*.js',
-  'jsDist': './dist/js/',
-  'ejsSrc': './ejs/*.ejs'
+  'html': './dist/index.html',
+  // 'cssSrc': './src/assets/scss/**/*.scss',
+  'imgSrc': './src/assets/img/**',
+  'jsSrc': './src/assets/js/**.js',
+  'ejsSrc': './ejs/*.ejs',
+  // 'cssDist': './dist/assets/css/',
+  'imgDist': './dist/assets/img/',
+  'jsDist': './dist/assets/js/'
 }
 
 //gulpコマンドの省略
-var { watch, series, task, src, dest, parallel } = require('gulp');
+const { watch, series, task, src, dest, parallel } = require('gulp');
 
 //Sass
 // task('sass', function () {
@@ -47,7 +53,7 @@ gulp.task('ejs', (done) => {
     .pipe(ejs({}, {}, { ext: '.html' }))
     .pipe(rename({ extname: '.html' }))
     .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, '$1'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest());
   done();
 });
 
@@ -79,12 +85,33 @@ task('reload', (done) => {
   done();
 });
 
+// imagemin
+gulp.task("imagemin", function () {
+  return gulp
+    .src(paths.imgSrc)
+    .pipe(changed(paths.imgDist))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [.60, .70], // 画質
+          speed: 1 // スピード
+        }),
+        mozjpeg({ quality: 65 }), // 画質
+        imagemin.svgo(),
+        imagemin.optipng(),
+        imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
+      ])
+    )
+    .pipe(gulp.dest(paths.imgDist));
+});
+
 //watch
 task('watch', (done) => {
   // watch([paths.cssSrc], series('sass', 'reload'));
   watch([paths.jsSrc], series('js', 'reload'));
   watch([paths.ejsSrc], series('ejs', 'reload'));
   watch([paths.html], gulp.task('reload'));
+  watch(['./src/assets/img/**'], gulp.task('imagemin'));
   done();
 });
 task('default', parallel('watch', 'browser-sync'));
